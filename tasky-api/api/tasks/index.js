a0280dfa66134898a827fa1ff9ce0d41cffaac05
Harry OShea <20107680@setu.ie>
2025-12-13 23:49:28 +0000

    Routes for get task and task details fixed

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -3,10 +3,6 @@ import { tasksData } from './tasksData';
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
-    res.json(tasksData);
-});
-
 // Get task details
 router.get('/:id', (req, res) => {
     const { id } = req.params
