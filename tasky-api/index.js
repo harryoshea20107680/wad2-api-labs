944839c307825ed10d2788810e89a8545eb1bada
Harry OShea <20107680@setu.ie>
2025-12-14 01:55:31 +0000

    Added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import cors from 'cors';
+import usersRouter from './api/users';
 
 
 dotenv.config();
@@ -26,6 +27,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
