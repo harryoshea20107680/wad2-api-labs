487e00a1aea114fe10c59920fe81b3951c39c9a3
Harry OShea <20107680@setu.ie>
2025-12-21 01:36:38 +0000

    Authentication front-end set up

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -12,6 +12,7 @@ dotenv.config();
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
+  
   if(process.env.NODE_ENV === 'production') {
     return res.status(500).send(`Something went wrong!`);
   }
---
+++ tasky-react/src/components/Task.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Task = (props) => {
+    
+    
+    return (
+    <div className={`card ${props.priority.toLowerCase()}`} style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+
+      <p className="title">{props.title}</p>
+      <p>Due: {props.deadline}</p>
+      <p className="description">{props.description}</p>
+      <p className={`priority ${props.priority.toLowerCase()}`}>
+        Priority: {props.priority}
+      </p>
+      <button onClick={props.markDone} className='doneButton'>Done</button>
+      <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+
+    </div>
+  );
+}
+
+
+
+
+export default Task;
